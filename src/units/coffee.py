12ce8d8ce6836448e4ef78970ba64afbef8609fb
jobatabs <2370315+jobatabs@users.noreply.github.com>
2023-04-03 23:30:30 +0300

    Add subclasses Coffee and Water

---
+++ src/units/coffee.py
@@ -0,0 +1,17 @@
+"""Defines Coffee, a child class of Unit that adds conversion to coffee spoons.
+"""
+
+from decimal import Decimal, ROUND_HALF_UP
+from units.unit import Unit
+
+class Coffee(Unit):
+    """A subclass of Unit that adds conversion to coffee spoons.
+    """
+    @property
+    def coffee_spoons(self) -> int:
+        """Calculates volume in amount of coffee spoons.
+
+        Returns:
+            int: Amount of coffee spoons, rounded to nearest full spoon.
+        """
+        return int((self._mass_g / 12).quantize(Decimal("0"), rounding=ROUND_HALF_UP))
---
+++ src/units/water.py
@@ -0,0 +1,35 @@
+"""Defines Water, a subclass of Unit that adds fluid volume conversions.
+"""
+
+from decimal import Decimal
+from units.unit import Unit
+
+class Water(Unit):
+    """Water supplements Unit with fluid volume conversions.
+    """
+    @property
+    def millilitres(self) -> Decimal:
+        """Calculates volume in millilitres.
+
+        Returns:
+            Decimal: Volume in millilitres.
+        """
+        return self._mass_g
+
+    @property
+    def fluid_ounces(self) -> Decimal:
+        """Calculates volume in US fluid ounces.
+
+        Returns:
+            Decimal: Volume in US fluid ounces.
+        """
+        return (self._mass_g / Decimal("29.57353")).quantize(Decimal("10.00000"))
+
+    @property
+    def litres(self) -> Decimal:
+        """Calculates volume in litres
+
+        Returns:
+            Decimal: Volume in litres.
+        """
+        return self._mass_g / 1000
