5cda33771e8565f70d4125193178226e5cfbfe86
jobatabs <2370315+jobatabs@users.noreply.github.com>
2023-04-03 23:31:41 +0300

    Add docstrings

--- src/index.py
+++ src/index.py
@@ -1,9 +1,14 @@
+"""Main executable file for the application.
+"""
+
 from tkinter import Tk
 
 def main():
+    """Main function that starts the application.
+    """
     window = Tk()
     window.title("ratio")
-    
+
     window.mainloop()
 
 if __name__ == "__main__":
