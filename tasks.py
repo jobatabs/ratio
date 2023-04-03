a07a3abcc742b20bf0027b638bc06968609d9b82
jobatabs <2370315+jobatabs@users.noreply.github.com>
2023-04-03 23:31:47 +0300

    Add docstrings

--- tasks.py
+++ tasks.py
@@ -1,17 +1,28 @@
+"""tasks.py sets up tasks for the invoke helper.
+"""
+
 from invoke import task
 
 @task
 def start(ctx):
+    """Starts the program.
+    """
     ctx.run("python3 src/index.py", pty=True)
 
 @task
 def test(ctx):
+    """Runs tests.
+    """
     ctx.run("pytest src", pty=True)
 
 @task
 def coverage(ctx):
+    """Generates coverage analysis.
+    """
     ctx.run("coverage run --branch -m pytest src", pty=True)
 
 @task(coverage)
 def coverage_report(ctx):
+    """Generates coverage html files.
+    """
     ctx.run("coverage html", pty=True)
