af88f1eaabd39fae33130c3cb5c93bb8c34a91c2
jobatabs <2370315+jobatabs@users.noreply.github.com>
2023-04-17 19:20:43 +0300

    Add tests for Recipe

---
+++ src/tests/units/recipe_test.py
@@ -0,0 +1,20 @@
+import unittest
+from decimal import Decimal
+from units.recipe import Recipe
+
+class TestRecipe(unittest.TestCase):
+    def setUp(self):
+        self.recipe_one_x = Recipe(one_x=Decimal(17))
+        self.recipe_grams_litre = Recipe(grams_litre=Decimal(65))
+
+    def test_grams_litre(self):
+        self.assertEqual(self.recipe_one_x.grams_litre, Decimal("58.82"))
+
+    def test_one_x(self):
+        self.assertEqual(self.recipe_grams_litre.one_x, Decimal("15.38"))
+
+    def test_overloaded_recipe(self):
+        self.assertRaises(TypeError, Recipe, Decimal(17), Decimal(65))
+
+    def test_empty_recipe(self):
+        self.assertRaises(TypeError, Recipe)
