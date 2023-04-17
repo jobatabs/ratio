a3a5cbf26093fb2b9c9502b013e5106d797d2603
jobatabs <2370315+jobatabs@users.noreply.github.com>
2023-04-17 21:06:36 +0300

    Test against fractions

--- src/tests/units/recipe_test.py
+++ src/tests/units/recipe_test.py
@@ -2,16 +2,19 @@ import unittest
 from decimal import Decimal
 from units.recipe import Recipe
 
+
 class TestRecipe(unittest.TestCase):
     def setUp(self):
         self.recipe_one_x = Recipe(one_x=Decimal(17))
         self.recipe_grams_litre = Recipe(grams_litre=Decimal(65))
 
     def test_grams_litre(self):
-        self.assertEqual(self.recipe_one_x.grams_litre, Decimal("58.82"))
+        self.assertEqual(self.recipe_one_x.grams_litre,
+                         Decimal(1000)/Decimal(17))
 
     def test_one_x(self):
-        self.assertEqual(self.recipe_grams_litre.one_x, Decimal("15.38"))
+        self.assertEqual(self.recipe_grams_litre.one_x,
+                         Decimal(1000)/Decimal(65))
 
     def test_overloaded_recipe(self):
         self.assertRaises(TypeError, Recipe, Decimal(17), Decimal(65))
