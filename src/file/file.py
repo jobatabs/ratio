797f217ba6e9c47737eafe8b85bbaa6983eaa8cc
jobatabs <2370315+jobatabs@users.noreply.github.com>
2023-05-13 17:50:51 +0300

    Add file I/O

---
+++ src/file/file.py
@@ -0,0 +1,24 @@
+import io
+from decimal import Decimal
+from units.coffee import Coffee
+from units.water import Water
+from units.recipe import Recipe
+
+
+def write_recipe(file: io.TextIOWrapper, coffee: Coffee, water: Water, recipe: Recipe) -> bool:
+    if file is not None:
+        file.write(f"{coffee.grams}\n")
+        file.write(f"{water.grams}\n")
+        file.write(f"{recipe.grams_litre}\n")
+        return True
+    return False
+
+
+def read_recipe(file: io.TextIOWrapper) -> tuple[Coffee, Water, Recipe]:
+    if file is not None:
+        lines = file.readlines()
+        coffee = Coffee(Decimal(lines[0]))
+        water = Water(Decimal(lines[1]))
+        recipe = Recipe(grams_litre=Decimal(lines[2]))
+        return coffee, water, recipe
+    return None
